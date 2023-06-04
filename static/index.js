4ef82f947f09b686bb9ee75f25acf2a80976b927
sudhanva <sudhanva0703@gmail.com>
2023-06-04 12:57:57 +0530

    updates folder structure

---
+++ static/index.js
@@ -0,0 +1,16 @@
+var v=Math.random();
+v=v*6;
+v=Math.floor(v)+1;
+var b=Math.floor((Math.random()*6))+1;
+var randomDice="images\\dice"+v+".png";
+var randomDice2="images\\dice"+b+".png";
+    document.querySelectorAll("img")[0].setAttribute("src",randomDice);
+    document.querySelectorAll("img")[1].setAttribute("src",randomDice2);
+
+if(v>b){
+    document.querySelector("h1").innerHTML="Player 1 won";
+}else if(v<b){
+    document.querySelector("h1").innerHTML="Player 2 won";
+}else{
+    document.querySelector("h1").innerHTML="It's a Draw";
+}
